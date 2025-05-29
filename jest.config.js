87876be40221cbf019b646413c1dd49e3c516b84
hsalinas <hsalinas@synack.com>
2025-05-29 10:50:57 -0400

    fix missing/unparsed env vars

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,12 @@ module.exports = {
         outputName: 'junit.xml',
       }],
       ['jest-qase-reporter', {
-        apiToken: process.env.QASE_API_TOKEN,
-        projectCode: process.env.QASE_PROJECT_CODE,
-        runId: process.env.QASE_RUN_ID,
+        apiToken: process.env.QASE_TESTOPS_API_TOKEN,
+        projectCode: process.env.QASE_TESTOPS_PROJECT,
+        runId: process.env.QASE_TESTOPS_RUN_ID,
+        runComplete: true,
+        baseUrl: process.env.QASE_API_BASE_URL || 'https://api.qase.io/v1',
+        enabled: process.env.QASE_REPORT === 'true',
       }],
     ],
   };
